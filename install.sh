0d6719751dd5bbda19543000ae4383e8397056b2
oscar <oscar2712@gmail.com>
2020-10-26 19:29:40 +0000

    Removed konsole

--- install.sh
+++ install.sh
@@ -6,9 +6,6 @@ ln -sfv $DIR/.gtkrc-2.0 ~
 ln -sfv $DIR/.gitignore_global ~
 ln -sfv $DIR/.config/i3/config ~/.config/i3/
 ln -sfv $DIR/.config/i3/i3status.conf ~/.config/i3/
-ln -sfv $DIR/.config/konsolerc ~/.config/konsolerc
-ln -sfv $DIR/DoMo.profile ~/.local/share/konsole/DoMo.profile
-ln -sfv $DIR/DarkPastels.colorscheme ~/.local/share/konsole/DarkPastels.colorscheme
 ln -sfv $DIR/wombat_oscar.vim ~/.vim/colors/wombat_oscar.vim
 ln -sfv $DIR/airline_theme/minimalist_domondo.vim ~/.vim/bundle/vim-airline-themes/autoload/airline/themes/minimalist_domondo.vim
 ln -sfv $DIR/.clang-format ~/.clang-format
