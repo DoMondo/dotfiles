595af27b99b4889a76eed7d3d64419f3102e16d3
ogomez <oscar2712@gmail.com>
2021-11-05 11:56:20 +0000

    Fixed install and added i3 keyboard resize mode

--- install.sh
+++ install.sh
@@ -6,8 +6,6 @@ ln -sfv $DIR/.gtkrc-2.0 ~
 ln -sfv $DIR/.gitignore_global ~
 ln -sfv $DIR/.config/i3/config ~/.config/i3/
 ln -sfv $DIR/.config/i3/i3status.conf ~/.config/i3/
-ln -sfv $DIR/wombat_oscar.vim ~/.vim/colors/wombat_oscar.vim
-ln -sfv $DIR/airline_theme/minimalist_domondo.vim ~/.vim/bundle/vim-airline-themes/autoload/airline/themes/minimalist_domondo.vim
 ln -sfv $DIR/.clang-format ~/.clang-format
 mkdir ~/.config/yapf
 ln -sfv $DIR/.style.yapf ~/.config/yapf/.style.yapf
@@ -23,6 +21,14 @@ mkdir $HOME/.vim
 mkdir $HOME/.vim/undo
 mkdir $HOME/.vim/backup
 mkdir $HOME/.vim/swapfiles
+mkdir $HOME/.vim/colors
+mkdir $HOME/.vim/bundle
+mkdir $HOME/.vim/bundle/vim-airline-themes
+mkdir $HOME/.vim/bundle/autoload
+mkdir $HOME/.vim/bundle/autoload/airline
+mkdir $HOME/.vim/bundle/autoload/airline/themes
+ln -sfv $DIR/wombat_oscar.vim ~/.vim/colors/wombat_oscar.vim
+ln -sfv $DIR/airline_theme/minimalist_domondo.vim ~/.vim/bundle/vim-airline-themes/autoload/airline/themes/minimalist_domondo.vim
 
 mkdir  $HOME/.local/share/fonts
 cp $DIR/fonts/* $HOME/.local/share/fonts
@@ -32,7 +38,6 @@ fc-cache -f $HOME/.local/share/fonts
 mkdir ~/lock-i3-desktop/
 ln -sfv $DIR/i3-exit ~/lock-i3-desktop/i3-exit
 git config --global core.excludesfile ~/.gitignore_global
-sudo pip install thefuck
 mkdir $HOME/.zfunctions
 
 ln -sfv $DIR/pure.zsh $HOME/.zfunctions/prompt_pure_setup
