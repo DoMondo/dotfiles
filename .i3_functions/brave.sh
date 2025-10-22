b25a384098cb83480c7421f75b9bdd65985e24d3
ogomez <oscar2712@gmail.com>
2025-10-22 11:47:42 +0100

    Changes mklatex to use xelatex and temporarily fixes brave crash
    https://github.com/hyprwm/Hyprland/discussions/11961

--- .i3_functions/brave.sh
+++ .i3_functions/brave.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-brave "$@" --enable-features=UseOzonePlatform --ozone-platform=wayland
+brave "$@" --enable-features=UseOzonePlatform --ozone-platform=wayland --disable-features=WaylandWpColorManagerV1
